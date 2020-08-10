687d217b6661bc41cc3aee95a98eabd56dadb47c
kathleenfrench <kfrench09@gmail.com>
2020-08-10 15:44:03 -0400

    rm unused

--- internal/helpers/helpers.go
+++ internal/helpers/helpers.go
@@ -7,19 +7,6 @@ func Spacer() {
 	fmt.Println("")
 }
 
-// func Sudo(cmd string) (string, error) {
-// 			// sudo check
-// 			a := exec.Command("sudo", "ls")
-// 			a.Stderr = os.Stderr
-// 			a.Stdin = os.Stdin
-// 			out, err := a.Output()
-// 			if err != nil {
-// 				gui.ExitWithError(err)
-// 			}
-
-// 			fmt.Println(string(out))
-// }
-
 // GetKeysFromMap is a helper for fetching a slice of strings from a mpa
 func GetKeysFromMap(m map[string]string) (keys []string) {
 	for k := range m {
