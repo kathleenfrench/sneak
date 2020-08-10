6538a99ab78e49188738a11c06a14951a027f4cc
kathleenfrench <kfrench09@gmail.com>
2020-08-09 20:57:29 -0400

    add dockerfiles

--- README.md
+++ README.md
@@ -4,4 +4,19 @@
 
 ### local db
 
-`sneak` uses `bolthold` (which wraps `boltdb`) to manage data locally beyond the user's custom configurations at the application level. if you want to interact with the database, there's a hidden command `sneak db` that will enable you to view the bucket(s), reset the database, as well as back it up.
+`sneak` uses `bolthold` (which wraps `boltdb`) to manage data locally beyond the user's custom configurations at the application level. if you want to interact with the database, there's a hidden command `sneak db` that will enable you to view the bucket(s), reset the database, as well as back it up
+
+
+#### running it in docker
+
+```
+	@docker run \
+		-v $(CWD)/shared/:/home/$(USERNAME)/tools \
+		-v $(CWD)/boxes/:/home/$(USERNAME)/boxes \
+		--privileged \
+		--sysctl net.ipv6.conf.all.disable_ipv6=0 \
+		--env LOCAL_NETWORK=192.168.1.151 \
+		-p 8118:8118 \
+		-it kathleenfrench/honcho \
+		/bin/bash
+```
--- internal/vpn/setup.go
+++ internal/vpn/setup.go
@@ -1,15 +1,20 @@
 package vpn
 
 import (
+	"fmt"
+	"io"
 	"io/ioutil"
+	"os"
 
+	"github.com/kathleenfrench/common/exec"
 	"github.com/kathleenfrench/common/fs"
 	"github.com/kathleenfrench/common/gui"
+	"github.com/mitchellh/go-homedir"
 )
 
 // AlreadySetup is a helper to check whether or not a user's openvpn configs have already been setup
 func (o *OpenVPN) AlreadySetup() bool {
-	if fs.FileExists(o.Filepath) {
+	if fs.FileExists(o.Filepath) && checkForPrivoxyConfig() {
 		return true
 	}
 
@@ -29,5 +34,72 @@ func (o *OpenVPN) Setup(defaultEditor string) error {
 		gui.ExitWithError(err)
 	}
 
+	// create privoxy config from default
+	err = createFunctionalPrivoxyConfig()
+	if err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func createFunctionalPrivoxyConfig() error {
+	whoami, err := exec.BashExec("whoami")
+	if err != nil {
+		return err
+	}
+
+	// need to include the confdir and logdir lines in the privoxy config file for it to work
+	appendLines := fmt.Sprintf("\n%s\n%s\n", fmt.Sprintf("confdir /home/%s", whoami), fmt.Sprintf("logdir /home/%s", whoami))
+
+	home, err := homedir.Dir()
+	if err != nil {
+		gui.ExitWithError(err)
+	}
+
+	defaultConfigs, err := os.Open(fmt.Sprintf("%s/config.default", home))
+	if err != nil {
+		return fmt.Errorf("could not locate the default privoxy configs - %s", err)
+	}
+
+	defer defaultConfigs.Close()
+
+	privoxyConfig, err := os.Create(fmt.Sprintf("%s/config", home))
+	if err != nil {
+		return err
+	}
+
+	defer privoxyConfig.Close()
+
+	_, err = io.Copy(privoxyConfig, defaultConfigs)
+	if err != nil {
+		return fmt.Errorf("could not write default privoxy file to new config file - %s", err)
+	}
+
+	privoxyConfigPreModify, err := os.OpenFile(fmt.Sprintf("%s/config", home), os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+
+	defer privoxyConfigPreModify.Close()
+	_, err = privoxyConfigPreModify.WriteString(appendLines)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func checkForPrivoxyConfig() bool {
+	home, err := homedir.Dir()
+	if err != nil {
+		gui.ExitWithError(err)
+	}
+
+	privoxyConfig := fmt.Sprintf("%s/config", home)
+	if fs.FileExists(privoxyConfig) {
+		return true
+	}
+
+	return false
 }
